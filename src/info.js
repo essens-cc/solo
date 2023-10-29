13de3dc603a401542a8febca98acceaaa7356898
0xShapeShifter <113918140+0xShapeShifter@users.noreply.github.com>
2023-10-29 07:56:05 +0300

    build: info file + moved images

---
+++ src/info.js
@@ -0,0 +1,69 @@
+import xDark from "./assets/social/dark/x.svg"
+import xLight from "./assets/social/light/x.svg"
+
+export const info = {
+    firstName: "Jensen",
+    secondName: "Lakes",
+    occupation: "Digital Director",
+    description: "Hi, I'm Jensen. I have over 10 years of experience as a digital director with a multidisciplinary background. My main goal is to create meaningful products that cause positive disruption.",
+    status: "⚡︎ Open for remote/async offers",
+    email: "✉︎ jensen@jensenlakes.com",
+    featuredWork: [
+        {
+            title: "Company X",
+            dates: "2010-2015"
+        },
+        {
+            title: "Company Y",
+            dates: "2015-2020"
+        },
+        {
+            title: "Company Z",
+            dates: "2020-now"
+        },
+    ],
+    recentProjects: [
+        {
+            title: "Project X",
+            url: "0xshapeshifter.dev"
+        },
+        {
+            title: "Project y",
+            url: "0xshapeshifter.dev"
+        },
+        {
+            title: "Project Z",
+            url: "0xshapeshifter.dev"
+        },
+    ],
+    recognition: [
+        {
+            title: "Award X",
+            date: "2010"
+        },
+        {
+            title: "Award Y",
+            date: "2015"
+        },
+        {
+            title: "Award Z",
+            date: "2020"
+        },
+    ],
+    socialMedia: [
+        {
+            title: "X",
+            url: "https://google.com",
+            images: {
+                dark: {
+                    src: xDark,
+                    alt: "X logo"
+                },
+                light: {
+                    src: xLight,
+                    alt: "X logo"
+                }
+            }
+        }
+    ]
+}
